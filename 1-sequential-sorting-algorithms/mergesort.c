38c9bad6d40b4a353208c793f2386a4405273db9
Arthur Wallendorff <hello@metaoist.io>
2023-08-14 23:03:51 +0200

    refactor according to the book's conventions

--- 1-sequential-sorting-algorithms/mergesort.c
+++ 1-sequential-sorting-algorithms/mergesort.c
@@ -14,14 +14,15 @@
 #define ARRAY_LENGTH 10
 
 // Basic utility function for debugging purposes
-void printArray(const int array[static 1], const size_t size, const char* msg) {
+void printArray(int const array[static 1], size_t const size,
+                char const msg[static 1]) {
     for (size_t i = 0; i < size; i++) {
         printf("%s[%zu]: %d\n ", msg, i, array[i]);
     }
 }
 
 // Simple test function
-bool isArraySorted(const int array[static 1], const size_t size) {
+bool isArraySorted(int const array[static 1], size_t const size) {
     // Finishing before the last element prevents undefined behavior
     for (size_t i = 1; i < size - 1; i++) {
         if (array[i - 1] > array[i] || array[i + 1] < array[i]) {
@@ -33,9 +34,9 @@ bool isArraySorted(const int array[static 1], const size_t size) {
 }
 
 // Returns both arrays as one merged (and sorted) array
-int* mergeArrays(const int a[static 1], const int b[static 1],
-                 const size_t aSize, const size_t bSize) {
-    const size_t bufferSize = aSize + bSize;
+int* mergeArrays(int const a[static 1], int const b[static 1],
+                 size_t const aSize, size_t const bSize) {
+    size_t const bufferSize = aSize + bSize;
     int* buffer = malloc(bufferSize * sizeof(int));
 
     // Each array has its own index variable
@@ -56,17 +57,17 @@ int* mergeArrays(const int a[static 1], const int b[static 1],
     return buffer;
 }
 
-int* mergeSort(int array[static 1], const size_t arraySize) {
+int* mergeSort(int const array[static 1], size_t const arraySize) {
     if (arraySize == 1) {
-        return array;
+        return (int*)array;
     }
 
     // If the given array size is odd,
     // we will add the remaining element to the second one
     // e.g.: [1,2,3,4,5] -> [1,2] [3,4,5]
-    const bool isArraySizeOdd = arraySize % 2 == 1;
-    const size_t firstArrayHalf = arraySize / 2;
-    const size_t secondArrayHalf = firstArrayHalf + (isArraySizeOdd ? 1 : 0);
+    bool const isArraySizeOdd = arraySize % 2 == 1;
+    size_t const firstArrayHalf = arraySize / 2;
+    size_t const secondArrayHalf = firstArrayHalf + (isArraySizeOdd ? 1 : 0);
 
     // The recursive calls to mergeSort will divide the given array until it is
     // made of one element, after that it will loop back to the mergeArrays
@@ -74,7 +75,7 @@ int* mergeSort(int array[static 1], const size_t arraySize) {
     // passing the full `array` with `firstArrayHalf` will just process the
     // first half of `array` `array + firstArrayHalf` is basic pointer
     // arithmetics that skip the first half of `array`
-    int* finalResult =
+    int* const finalResult =
         mergeArrays(mergeSort(array, firstArrayHalf),
                     mergeSort(array + firstArrayHalf, secondArrayHalf),
                     firstArrayHalf, secondArrayHalf);
@@ -83,7 +84,7 @@ int* mergeSort(int array[static 1], const size_t arraySize) {
 }
 
 int main(void) {
-    int arr[ARRAY_LENGTH] = {5, 1, -6, 13, 742, 2, 6, 7, 1, 3};
+    int const arr[ARRAY_LENGTH] = {5, 1, -6, 13, 742, 2, 6, 7, 1, 3};
 
     int* sorted = mergeSort(arr, ARRAY_LENGTH);
 
